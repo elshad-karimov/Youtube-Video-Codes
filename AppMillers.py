03799f5d5980e765cd08acd4e4cfef4604eb9994
Elshad <elshad040@gmail.com>
2024-07-05 18:44:37 +0400

    Initial commit

---
+++ AppMillers.py
@@ -0,0 +1,12 @@
+#  Created by Elshad Karimov 
+#  Copyright © 2024 AppMillers.
+#  All rights reserved. 
+
+text = "appmillers.com"
+
+print(f"{text}")
+print(f"{text:*<30}")
+print(f"{text:@>30}")
+print(f"{text:.^30}")
+
+
